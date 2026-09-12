34a30549eba77b869a8a88d605787c5e300081d5
Emilye Hollins <emilyehollins@MacBook-Air-3.local>
2026-09-12 16:43:48 -0500

    Integrate Stacked backend with Gemini and Nessie

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-
 import os
 
 import dotenv
+from fastapi import FastAPI
+from pydantic import BaseModel
 from google import genai
+
+from stacked_backend import router as stacked_router
+
 
 dotenv.load_dotenv()
 
+app = FastAPI()
+
+
+# Connect Emilye's Stacked backend
+app.include_router(stacked_router)
+
+
+# Connect Gemini
 client = genai.Client(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
-while True: 
-    userInput = input("You: ")
-    if userInput.lower() == "exit":
-        break
+
+class ChatMessage(BaseModel):
+    message: str
+
+
+@app.post("/chat")
+def chat(chat_message: ChatMessage):
+
     response = client.models.generate_content(
         model="gemini-3.5-flash-lite",
-        contents=userInput
+        contents=chat_message.message
     )
-    print(f"\nAI Chatbot: {response.text}")
-    
+
+    return {
+        "response": response.text
+    }
